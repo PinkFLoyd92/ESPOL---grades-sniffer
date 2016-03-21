a56250d0341a9d3a5e3d8cd9df84108b8b732830
PinkFloyd92 <ascacere92@gmail.com>
2016-03-20 23:39:18 -0500

    agregado spinner temporal hasta hacer el listview, ya se muestran datos de usuarios.

--- app/src/main/java/Classes/StalkUsers/Estudiante.java
+++ app/src/main/java/Classes/StalkUsers/Estudiante.java
@@ -4,6 +4,38 @@ package Classes.StalkUsers;
  * Created by sebas on 3/20/16.
  */
 public class Estudiante {
+    public String getCodigo_estudiante() {
+        return codigo_estudiante;
+    }
+
+    public void setCodigo_estudiante(String codigo_estudiante) {
+        this.codigo_estudiante = codigo_estudiante;
+    }
+
+    public String getNumero_identificacion() {
+        return numero_identificacion;
+    }
+
+    public void setNumero_identificacion(String numero_identificacion) {
+        this.numero_identificacion = numero_identificacion;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
     private String codigo_estudiante,numero_identificacion,nombres, apellidos;
 
     public Estudiante(String codigo_estudiante, String numero_identificacion, String nombres, String apellidos) {
--- app/src/main/java/com/fenonimous/polstalk/MainActivity.java
+++ app/src/main/java/com/fenonimous/polstalk/MainActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -13,27 +17,45 @@ import Classes.StalkUsers.Estudiante;
 import Classes.WebService;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText nombres;
+    EditText apellidos;
+    EditText matricula;
+    Spinner dropdown_estudiantes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nombres = (EditText)findViewById(R.id.nombres);
+        apellidos = (EditText)findViewById(R.id.apellidos);
+        dropdown_estudiantes = (Spinner)findViewById(R.id.spinner_estudiantes);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.actionbar_principal);
         setSupportActionBar(myToolbar);
-        WebService soap = new WebService();
-        soap.setNombre_a_buscar("andres");
-        soap.setApellido_a_buscar("caceres");
-        soap.setOpcion_escogida("wsConsultarPersonaPorNombres"); // ponemos el nombre del metodo.
-        //soap.setOpcion_escogida("HelloWorld");
-        soap.start();
-       while(soap.getRun_state()!=1);
-        ArrayList<Estudiante> estudiantes = soap.getEstudiantes_from_consulta();
-        Log.d("count",estudiantes.size()+"");
+
+
         //String response = soap.getWebResponse();
         //Log.d("app",response);
 
     }
+    //click en boton submit de consultar.
+    public void consultarPersonaPorNombres(View view){
+        WebService soap = new WebService();
+        soap.setNombre_a_buscar(nombres.getText().toString());
+        soap.setApellido_a_buscar(apellidos.getText().toString());
+        soap.setOpcion_escogida("wsConsultarPersonaPorNombres"); // ponemos el nombre del metodo.
+        //soap.setOpcion_escogida("HelloWorld");
+        soap.start();
+        while(soap.getRun_state()!=1);
+        ArrayList<Estudiante> estudiantes = soap.getEstudiantes_from_consulta();
+        ArrayList<String> estudiantes_nombres = new ArrayList<>();
+        for (Estudiante e: estudiantes) {
+            estudiantes_nombres.add(e.getNombres());
+        }
+        ArrayAdapter<String> student_adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, estudiantes_nombres);
+                dropdown_estudiantes.setAdapter(student_adapter);
+                dropdown_estudiantes.setSelection(0);
 
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
